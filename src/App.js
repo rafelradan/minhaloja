8ef323361122e699aee854fd8c16a72e261ca80e
Rafael Nascimento <rafaelradan76@gmail.com>
2022-01-05 02:53:29 -0300

    Add the HTML

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import './globalStyle.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="minhaloja">
+          <span>Minha Loja</span>
+        </div>
+
+        <div className="search">
+          <input className="search-inp">
+            <button className="filter">Filtar</button>
+
+            <button className="clean">X</button>
+          </input>
+        </div>
+
+        <div className="car">
+          <div className="img-car">
+            <img src="" alt="car" />
+          </div>
+
+          <div className="text-car">
+            <span>Carrinho</span>
+            <span>3 produtos</span>
+          </div>
+        </div>
       </header>
+
+      <section className='body'>
+        <div className='card'>
+          <div className='title-card'>
+            <h5>MOUSE SEM FIO BLUETOOTH</h5>
+          </div>
+
+          <div className='img-card'>
+            <img src='' alt='mouse' />
+          </div>
+        </div>
+
+        <div className='bottom-card'>
+          <span>R$ 85,00</span>
+          <button className='btn-card'>ADICIONAR</button>
+        </div>
+
+        <div className='card'>
+          <div className='title-card'>
+            <h5>TECLADO SEM FIO BLUETOOTH</h5>
+          </div>
+
+          <div className='img-card'>
+            <img src='' alt='teclado' />
+          </div>
+        </div>
+
+        <div className='bottom-card'>
+          <span>R$ 150,00</span>
+          <button className='btn-card'>ADICIONAR</button>
+        </div>
+
+        <div className='card'>
+          <div className='title-card'>
+            <h5>FONE SEM FIO BLUETOOTH</h5>
+          </div>
+
+          <div className='img-card'>
+            <img src='' alt='fone' />
+          </div>
+        </div>
+
+        <div className='bottom-card'>
+          <span>R$ 97,50</span>
+          <button className='btn-card'>ADICIONAR</button>
+        </div>
+
+      </section>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
