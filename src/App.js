8ef6766eaa4f82646bbcacdaaf3409c355ccc910
Rafael Nascimento <rafaelradan76@gmail.com>
2022-01-05 06:54:06 -0300

    Added the product cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './globalStyle.scss'
 import ImgClean from './img/imgClean.svg'
 import ImgCar from './img/imgCar.svg'
 
+import ImgMouse from './img/imgMouse.svg'
+import ImgKeyboard from './img/imgKeyboard.svg'
+import ImgHeadset from './img/imgHeadset.svg'
+
 function App() {
   return (
     <div className="App">
@@ -32,49 +36,51 @@ function App() {
       </header>
 
       <section className="body">
-        <div className="card">
-          <div className="title-card">
-            <h5>MOUSE SEM FIO BLUETOOTH</h5>
+        <div className="contents">
+          <div className="product-card">
+            <div className="title-card">
+              <h5>MOUSE SEM FIO BLUETOOTH</h5>
+            </div>
+
+            <div className="img-card">
+              <img src={ImgMouse} alt="mouse" />
+            </div>
+
+            <div className="bottom-card">
+              <span>R$ 85,00</span>
+              <button className="btn-card">ADICIONAR AO CARRINHO</button>
+            </div>
           </div>
 
-          <div className="img-card">
-            <img src="" alt="mouse" />
-          </div>
-        </div>
+          <div className="product-card">
+            <div className="title-card">
+              <h5>TECLADO SEM FIO BLUETOOTH</h5>
+            </div>
 
-        <div className="bottom-card">
-          <span>R$ 85,00</span>
-          <button className="btn-card">ADICIONAR</button>
-        </div>
+            <div className="img-card">
+              <img src={ImgKeyboard} alt="keyboard" />
+            </div>
 
-        <div className="card">
-          <div className="title-card">
-            <h5>TECLADO SEM FIO BLUETOOTH</h5>
-          </div>
-
-          <div className="img-card">
-            <img src="" alt="teclado" />
-          </div>
-        </div>
-
-        <div className="bottom-card">
-          <span>R$ 150,00</span>
-          <button className="btn-card">ADICIONAR</button>
-        </div>
-
-        <div className="card">
-          <div className="title-card">
-            <h5>FONE SEM FIO BLUETOOTH</h5>
+            <div className="bottom-card">
+              <span>R$ 150,00</span>
+              <button className="btn-card">ADICIONAR AO CARRINHO</button>
+            </div>
           </div>
 
-          <div className="img-card">
-            <img src="" alt="fone" />
-          </div>
-        </div>
+          <div className="product-card">
+            <div className="title-card">
+              <h5>FONE SEM FIO BLUETOOTH</h5>
+            </div>
 
-        <div className="bottom-card">
-          <span>R$ 97,50</span>
-          <button className="btn-card">ADICIONAR</button>
+            <div className="img-card">
+              <img src={ImgHeadset} alt="headset" />
+            </div>
+
+            <div className="bottom-card">
+              <span>R$ 97,50</span>
+              <button className="btn-card">ADICIONAR AO CARRINHO</button>
+            </div>
+          </div>
         </div>
       </section>
     </div>
